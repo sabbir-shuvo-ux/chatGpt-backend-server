bef04a56be54d96c972b734eee51c71b51ce74e8
sabbir-shuvo-ux <shuvosabbir134@gmail.com>
2023-03-05 20:51:47 +0600

    fix api

--- server.js
+++ server.js
@@ -9,56 +9,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = 8800 || process.env.PORT;
 
-const PORT = 8800 || process.env.PORT
-
-console.log(process.env.OPEN_API_KEY);
 const configuration = new Configuration({
-    organization: "org-HbijweiCAWlYI5LTtfpKd3MT",
-    apiKey: process.env.OPEN_API_KEY,
+  organization: "org-HbijweiCAWlYI5LTtfpKd3MT",
+  apiKey: process.env.OPEN_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).send({
+      message: "helllo",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+app.post("/api", async (req, res) => {
+  try {
+    const { prompt } = req.body;
 
-app.get('/', async (req, res)=>{
-    try {
-        res.status(200).send({
-            message: "helllo"
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
 
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err });
+  }
+});
 
-app.post('/api', async (req, res) => {
-    try {
-        const {prompt} = req.body;
-
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-          });
-
-          res.status(200).send({
-            bot: response.data.choices[0].text
-          })
-    } catch (err) {
-        console.log(err);
-        res.status(500).send({err})
-    }
-})
-
-app.listen(PORT, ()=>{
-    console.log(`Server is runing on port http://localhost:${PORT}`);
-})
-
-
-
+app.listen(PORT, () => {
+  console.log(`Server is runing on port http://localhost:${PORT}`);
+});
